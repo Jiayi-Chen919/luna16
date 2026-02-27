6a73d99cae8e9c76468fe4a34b4a8b858ef309c1
Jiayi-Chen919 <145276284+Jiayi-Chen919@users.noreply.github.com>
2026-02-27 17:11:37 +0800

    1

--- prepare/run_preprocess.py
+++ prepare/run_preprocess.py
@@ -1,4 +1,11 @@
 import os
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
+
 from ast import literal_eval
 from glob import glob
 import math
