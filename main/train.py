29537710725e4e15ae46b3c888d1b9d4d463361a
Jiayi-Chen919 <145276284+Jiayi-Chen919@users.noreply.github.com>
2026-02-27 17:08:06 +0800

    1

--- main/train.py
+++ main/train.py
@@ -1,9 +1,16 @@
+import os
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+  
 import random
 
 import torch
 import numpy as np
 import time
-import os
+
 from model.net import Net
 from model.loss import Loss
 from torch.autograd import Variable
